09ba84f9acfbbf2c93ba0a1ea48963ce1a9cde34
Christoffer Lind Andersen <chri692u@gmail.com>
2022-12-06 17:01:18 +0100

    New solutions

---
+++ AOC2022/3.cpp
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+    //Rank based on index + 1
+    std::string ranks = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::vector<char> item_types;
+    int sum = 0;
+    //Backpack input
+    std::vector<std::string> backpack = {"vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                                         "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                                         "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"};
+
+    //Find matching items in compartments
+    for (std::string str : backpack){
+        int i = 0, mid = str.size() / 2, j = mid;
+        while(true){
+            if (str.at(i) == str.at(j)){
+                item_types.push_back(str.at(i));
+                break;
+            } else if (j == str.size()-1) {
+                j = mid;
+                ++i;
+            }
+            ++j;
+        }
+    }
+
+    //Compute sum of priority values
+    for(char ch : item_types){
+        for(int k = 0; k < ranks.size(); ++k){
+            if (ch == ranks.at(k)){
+                sum += k+1;
+            }
+        }
+    }
+    std::cout << "Sum of priority items: " << sum << std::endl;
+
+    return EXIT_SUCCESS;
+}
