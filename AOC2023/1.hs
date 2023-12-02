49f7fab08e673eaefdad87f6cac5aba46ba2fe61
Christoffer Lind Andersen <57995582+Chri692u@users.noreply.github.com>
2023-12-02 20:31:59 +0100

    solution 1

--- AOC2023/1.hs
+++ AOC2023/1.hs
@@ -1,14 +1,20 @@
 module One where
 import Data.Char
+import Data.List
 
-solve :: [[Char]] -> Int
+getInput :: IO [String]
+getInput = do
+        content <- readFile "./inputs/1.txt"
+        return (lines content)
+
+solve :: [String] -> Int
 solve xs = sum $ map (read . adjust) strs :: Int
-    where
-        strs = map (filter isDigit) xs
-        adjust s = case length s of
-            1 -> s ++ s
-            _ -> head s : [last s]
+        where
+                strs = map (filter isDigit) xs
+                adjust s = case length s of
+                        1 -> s ++ s
+                        _ -> head s : [last s]
 
 main = do
-    let xs = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-    print $ solve xs
+        xs <- getInput
+        print $ solve xs
