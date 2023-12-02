2c4a59866df17ebda361e679286eec729e62083a
Christoffer Lind Andersen <57995582+Chri692u@users.noreply.github.com>
2023-12-02 20:32:06 +0100

    solution 2

---
+++ AOC2023/2.hs
@@ -0,0 +1,65 @@
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Data.Maybe (listToMaybe)
+import qualified Data.Text as T
+
+data Color = Blue | Green | Red deriving (Show)
+
+data Game = Game { gameId :: Int
+                                 , maxBlue :: Maybe Int
+                                 , maxGreen :: Maybe Int
+                                 , maxRed :: Maybe Int
+                                 } deriving (Show)
+
+gameIdParser :: Parser Int
+gameIdParser = do
+    string "Game "
+    id <- many1 digit
+    char ':'
+    spaces
+    return $ read id
+
+colorCounts = do
+    count <- many1 digit
+    space
+    color <- string "blue" <|> string "green" <|> string "red"
+    optional (char ',' <|> char ';')
+    spaces
+    return (color, read count)
+
+colorCountParser :: Parser (Maybe Int, Maybe Int, Maybe Int)
+colorCountParser = do
+    xs <- many1 colorCounts
+    let maxBlue = maximumMay [c | ("blue", c) <- xs]
+        maxGreen = maximumMay [c | ("green", c) <- xs]
+        maxRed = maximumMay [c | ("red", c) <- xs]
+    return (maxBlue, maxGreen, maxRed)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay xs = Just (maximum xs)
+
+gameParser :: Parser Game
+gameParser = do
+  id <- gameIdParser
+  (blue, green, red) <- colorCountParser
+  return $ Game id blue green red
+
+gamesParser :: Parser [Game]
+gamesParser = many1 gameParser
+
+main :: IO ()
+main = do
+    input <- readFile "./inputs/2.txt"
+    -- split string on \n
+    let games =  parse gamesParser "" input
+    case games of
+        Left err -> print err
+        Right games -> do
+            -- filter the games that have color values below or equal to the limit
+            let bl = Just 14
+            let gl = Just 13
+            let rl = Just 12
+            let filtered_games = filter (\(Game _ b g r) -> b <= bl && g <= gl && r <= rl) games
+            -- sum the game ids
+            print $ sum $ map gameId filtered_games
